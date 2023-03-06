086029d33fdd4864413c3d67d3d2fc22e961bc8f
Edmilson <krakadianbill@gmail.com>
2023-03-06 13:32:51 -0300

    feat: initial userRoutes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+const userRoutes: Router = Router()
+
+userRoutes.post('')
+userRoutes.get('')
+userRoutes.patch('/:id')
+userRoutes.delete('/:id')
+
+export default userRoutes
