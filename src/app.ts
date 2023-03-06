79e7de6a04a4c21c821e0a72a5d53b50f7696e1b
Edmilson <krakadianbill@gmail.com>
2023-03-06 13:34:25 -0300

    feat: login route added

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import 'express-async-errors'
 import express, { Application } from 'express'
 import { handleErrors } from './errors'
 import userRoutes from './routes/users.routes'
+import loginRoute from './routes/login.routes'
 
 const app: Application = express()
 app.use(express.json())
 app.use('/users', userRoutes)
+app.use('/login', loginRoute)
 app.use(handleErrors)
 export default app
---
+++ src/routes/login.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+
+const loginRoute: Router = Router()
+
+loginRoute.post('')
+
+export default loginRoute
