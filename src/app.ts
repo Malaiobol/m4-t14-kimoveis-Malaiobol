cd6eba4219566867b056c2f2e116e0e6c1b97c69
Edmilson <krakadianbill@gmail.com>
2023-03-06 13:39:01 -0300

    feat: initial mask for realEstate routes

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import { handleErrors } from './errors'
 import userRoutes from './routes/users.routes'
 import loginRoute from './routes/login.routes'
 import categoriesRoutes from './routes/categories.routes'
+import realEstateRoutes from './routes/realEstate.routes'
 
 const app: Application = express()
 app.use(express.json())
 app.use('/users', userRoutes)
 app.use('/login', loginRoute)
 app.use('/categories', categoriesRoutes)
+app.use('/realEstate', realEstateRoutes)
 app.use(handleErrors)
 export default app
