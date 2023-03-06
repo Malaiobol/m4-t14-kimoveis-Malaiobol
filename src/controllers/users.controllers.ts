df453ad2e9d55a34fdfe34fe7cfd58879ecd029e
Edmilson <krakadianbill@gmail.com>
2023-03-06 15:20:31 -0300

    feat: initial mask for users controllers

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express'
+import { IUser, IUserUpdate } from '../interfaces'
+
+const createUserController = async ( req: Request, res: Response ) => {
+    
+    const userData: IUser = req.body
+
+    const newUser = await (userData)
+
+    return res.status(201).json(newUser)
+}
+
+const listUsersController = async ( req: Request, res: Response ) => {
+
+    const allUsers = await 
+
+    return res.status(201).json(allUsers)
+}
+
+const updateUserController = async ( req: Request, res: Response ) => { 
+
+    const userData: IUserUpdate = req.body
+    const idUser: number = parseInt(req.params.id)
+
+    const updatedUser = await (userData, idUser)
+
+    return res.status(200).json(updatedUser)
+}
+
+const softDeleteUserController = async ( req: Request, res: Response ) => {
+    const idUser: number = parseInt(req.params.id)
+    await (idUser)
+
+    return res.status(204).send()
+}
+
+export {
+    createUserController,
+    listUsersController,
+    updateUserController,
+    softDeleteUserController
+}
