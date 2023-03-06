af568827dfc646827dea154045b9509b49381e4a
Edmilson <krakadianbill@gmail.com>
2023-03-06 14:42:20 -0300

    feat: export and import users interfaces to index

---
+++ src/interfaces/index.ts
@@ -0,0 +1,13 @@
+import {
+    IUser,
+    IUserReturn,
+    IUsersReturn,
+    IUserUpdate
+} from './users.interfaces'
+
+export {
+    IUser,
+    IUserReturn,
+    IUsersReturn,
+    IUserUpdate
+}
