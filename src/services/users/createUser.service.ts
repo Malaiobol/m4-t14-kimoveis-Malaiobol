17b7d00f503e7356a2bbc452ca4cf044e740cc3d
Edmilson <krakadianbill@gmail.com>
2023-03-06 15:30:08 -0300

    feat: added createUser service

---
+++ src/services/users/createUser.service.ts
@@ -0,0 +1,18 @@
+import { IUser, IUserReturn } from '../../interfaces'
+import { AppDataSource } from '../../data-source'
+import { User } from '../../entities'
+import { Repository } from 'typeorm'
+import { returnUserSchema } from '../../schemas/users.schemas'
+
+const createUserService = async (userData: IUser): Promise<IUserReturn> =>{
+    
+    const userRepository: Repository<User> = AppDataSource.getRepository(User)
+
+    const user: User = userRepository.create(userData)
+
+    const newUser = returnUserSchema.parse(user)
+
+    return newUser
+}
+
+export default createUserService
