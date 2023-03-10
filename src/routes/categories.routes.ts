9928eff2fe780fb9f52a6992e1cbefe0f888fa3c
Edmilson <krakadianbill@gmail.com>
2023-03-10 00:21:07 -0300

    feat: new verify middleware && feat: middlewares added in category routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,10 +3,13 @@ import {
     createCategoryController, 
     getAllCategoriesController 
 } from '../controllers/categories.controllers'
+import ensureDevIsAdminMiddleware from '../middlewares/ensureDevIsAdmin.middleware'
+import ensureTokenIsValidMiddleware from '../middlewares/ensureTokenIsValid.middleware'
+import ensureNameIsUnique from '../middlewares/verifyCategoryName.middleware'
 
 const categoriesRoutes: Router = Router()
 
-categoriesRoutes.post('', createCategoryController)
+categoriesRoutes.post('', ensureTokenIsValidMiddleware, ensureDevIsAdminMiddleware, ensureNameIsUnique, createCategoryController)
 categoriesRoutes.get('', getAllCategoriesController)
 categoriesRoutes.get('/:id/realEstate')
 
