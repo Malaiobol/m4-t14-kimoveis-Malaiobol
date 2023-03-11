0dd177d8f7cd441dde693c35f97f0726962314ee
Edmilson <krakadianbill@gmail.com>
2023-03-11 14:59:34 -0300

    feat: verification middlewares added (token and admin verifies)

--- src/routes/realEstate.routes.ts
+++ src/routes/realEstate.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
-import ensureDataIsValidMiddleware from '../middlewares/validateData.middleware'
 import { realEstateSchema } from '../schemas/realEstate.schemas'
-import { 
-    createRealEstateController, 
-    getAllRealEstates 
-} from '../controllers/realEstate.controllers'
+import { createRealEstateController, getAllRealEstates } from '../controllers/realEstate.controllers'
+
+import ensureDataIsValidMiddleware from '../middlewares/validateData.middleware'
+import ensureTokenIsValidMiddleware from '../middlewares/ensureTokenIsValid.middleware'
+import ensureDevIsAdminMiddleware from '../middlewares/ensureDevIsAdmin.middleware'
+
 const realEstateRoutes: Router = Router()
 
-realEstateRoutes.post('',ensureDataIsValidMiddleware(realEstateSchema), createRealEstateController)
+realEstateRoutes.post('',ensureTokenIsValidMiddleware, ensureDevIsAdminMiddleware, ensureDataIsValidMiddleware(realEstateSchema), createRealEstateController)
 realEstateRoutes.get('', getAllRealEstates)
 
 export default realEstateRoutes
