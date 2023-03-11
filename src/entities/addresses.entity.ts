90442ec439e369b4df77476fddcc5b8171555afc
Edmilson <krakadianbill@gmail.com>
2023-03-11 12:37:07 -0300

    feat: new addresses entity added

---
+++ src/entities/addresses.entity.ts
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn
+} from 'typeorm'
+
+@Entity('addresses')
+class Address {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number
+
+    @Column({ length: 45})
+    street: string
+
+    @Column({ length: 8 })
+    zipCode: string 
+
+    @Column({ length: 7, nullable: true, type:'varchar' })
+    number?: string | null | undefined
+
+    @Column({ length: 20 })
+    city: string
+    
+    @Column({ length: 2 })
+    state: string
+}
